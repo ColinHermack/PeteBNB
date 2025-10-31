a9d9cfa061a020b08453b7358bd27e82a8801d4d
Colin Hermack <chermack@purdue.edu>
2025-10-31 17:37:45 -0400

    Added listing data methods

--- src/data/listings.ts
+++ src/data/listings.ts
@@ -19,3 +19,56 @@ export async function getListing(id: string): Promise<Listing | null> {
         await client.close();
     }
 }
+
+export async function getAllListings(): Promise<Listing[]> {
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        const db = client.db("pete-bnb");
+        const collection = db.collection("Listings");
+        const result = await collection.find({}).toArray();
+        return result;
+    } catch (error) {
+        console.error(error);
+        return [];
+    } finally {
+        await client.close();
+    }
+}
+
+export async function addNewListing(listerUsername: string, title: string, description: string, price: number, startDate: Date, endDate: Date): Promise<boolean> {
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        const db = client.db("pete-bnb");
+        let collection = db.collection("Users");
+        const listerId = (await collection.findOne({ username: listerUsername })).userID;
+        collection = db.collection("Listings");
+        await collection.insertOne({ id: crypto.randomUUID(), lister: listerId, title: title, description: description, price: price, startdate: startDate, enddate: endDate });
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    } finally {
+        await client.close();
+    }
+}
+
+export async function deleteListing(id: string): Promise<boolean> {
+    const client = new MongoClient(uri);
+
+    try {
+        await client.connect();
+        const db = client.db("pete-bnb");
+        const collection = db.collection("Listings");
+        await collection.deleteOne({ id: id });
+        return true;
+    } catch (error) {
+        console.error(error);
+        return false;
+    } finally {
+        await client.close();
+    }
+}
